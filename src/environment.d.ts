4d72c032f15c55e3c5faed86a0935cf323103f7d
Geneza2 <info@geneza.rs>
2025-07-30 12:39:36 +0200

    GEN-0003 secret added to globals

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,7 +2,11 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PAYLOAD_SECRET: string
+      PREVIEW_SECRET: string
+      CRON_SECRET: string
       DATABASE_URI: string
+      POSTGRES_URL: string
+      BLOB_READ_WRITE_TOKEN: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
     }
